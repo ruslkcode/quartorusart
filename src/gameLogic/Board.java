30fde949cf342120f09338a22d94379ef528b87f
Artem Novokhatnii <artemnovokhatnii@gmail.com>
2026-01-19 15:22:26 +0100

    Board class with Constructor.

--- src/gameLogic/Board.java
+++ src/gameLogic/Board.java
@@ -4,8 +4,16 @@ public class Board {
     private final int DIM = 4 * 4;
     private Piece[] fields;
 
+    /**
+     * Constructs the Board object.
+     */
     public Board(){
-        
+        this.fields = new Piece[DIM * DIM];
+        for (Piece field: this.fields) {
+            field = null;
+        }
     }
+
+
 
 }
